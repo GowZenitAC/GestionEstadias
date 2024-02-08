bd87f66197b42042db81e460b2088337b3c4e4c5
noecan <noecanyam8@gmail.com>
2024-02-08 17:21:10 -0600

    ajuste de tabler

--- database/migrations/2024_01_23_190915_equipos.php
+++ database/migrations/2024_01_23_190915_equipos.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('equipos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-          
+            $table->string('nombre'); 
         });
     }
 
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('equipos');
     }
 };
